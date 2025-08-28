999937ff8523a1d5841970d6cbf85c6629cbca77
pedromartinspe07 <pedromartinspe07@gmail.com>
2025-08-28 12:19:05 -0300

    Atualização 0.1

---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // Configura os caminhos dos arquivos onde o Tailwind irá procurar por classes.
+    // Certifique-se de que isso inclui todos os seus arquivos de componente React.
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
